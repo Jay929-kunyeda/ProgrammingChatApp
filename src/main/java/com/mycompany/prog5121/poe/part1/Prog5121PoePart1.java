878bb0c60b66848bc3159f8972108c22b0213c2b
tinas <tinas@JaysPC>
2026-09-16 10:44:36 +0200

    I just finished linking my project to my Github respitory

---
+++ src/main/java/com/mycompany/prog5121/poe/part1/Prog5121PoePart1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121.poe.part1;
+
+/**
+ *
+ * @author tinas
+ */
+public class Prog5121PoePart1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
