7a7f57b3425db82b0568fa06938fb94bc9365b48
tinas <tinas@JaysPC>
2026-09-17 14:25:12 +0200

    I just ran my program and i have successfully been able to register my details to be a user

--- src/main/java/com/mycompany/prog5121/poe/part1/Prog5121PoePart1.java
+++ src/main/java/com/mycompany/prog5121/poe/part1/Prog5121PoePart1.java
@@ -1,16 +1,99 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.prog5121.poe.part1;
 
-/**
- *
- * @author tinas
- */
+
+
+import java.util.Scanner;
+
 public class Prog5121PoePart1 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+     public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+                     System.out.println
+        ("======================================");
+                System.out.println
+          (" Registration and Login Application");
+                    System.out.println
+        ("======================================");
+
+            System.out.println
+                         ("\n--- Registration ---");
+
+            System.out.print
+                        ("Enter your first name: ");
+            String firstName = scanner.nextLine();
+
+            System.out.print
+                        ("Enter your last name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.print
+                        ("Enter a username: ");
+            String username = scanner.nextLine();
+
+            System.out.print
+                        ("Enter a password: ");
+            String password = scanner.nextLine();
+
+            System.out.print
+                        ("Enter your cellphone number, "
+                    + "for example +27838968976: ");
+            
+                    
+       String cellPhone = scanner.nextLine();
+
+            Login user = new Login(
+                         firstName,
+                    lastName,
+                        username,
+                     password,
+                    cellPhone
+                                 );
+
+       String registrationMessage = user.registerUser();
+
+            
+       
+     if (user.isRegistered()) {
+                System.out.println
+            ("\nUsername successfully captured.");
+                System.out.println
+             ("Password successfully captured.");
+                System.out.println
+        ("Cell phone number successfully added.");
+                                 }
+
+            
+     System.out.println(registrationMessage);
+
+            if (!user.isRegistered())
+                    {
+                System.out.println
+        ("\nRegistration was unsuccessful.");
+                System.out.println
+        ("Please correct the details and try again.");
+                return;
+                        }
+
+     System.out.println
+        ("\n--- Login ---");
+
+    System.out.print
+            ("Enter your username to login: ");
+            String loginUsername = scanner.nextLine();
+
+    System.out.print
+            ("Enter your password to login: ");
+            String loginPassword = scanner.nextLine();
+
+             boolean loginSuccessful = user.loginUser
+                              (
+                      loginUsername,
+                         loginPassword
+                              );
+
+    System.out.println(user.returnLoginStatus(loginSuccessful));
+        }
     }
 }
